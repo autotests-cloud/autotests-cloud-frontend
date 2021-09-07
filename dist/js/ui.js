0f427de11d5eecc1bd19027b3b00c718476923c4
heycisco <mail@starchenkov.pro>
2021-09-07 18:11:47 +0300

    Кнопки редактирования тест-кейсов

--- dist/js/ui.js
+++ dist/js/ui.js
@@ -25,10 +25,10 @@ const informationSection = document.querySelector(".information");
 const informationTrigger = document.querySelector(".information .trigger");
 const mainFormSection = document.querySelector(".main-block__page");
 const mainFormSectionTrigger = document.querySelector(
-  ".start .wrapper .options"
+	".start .wrapper .options"
 );
 const mainFormSectionTrigger2 = document.querySelector(
-  ".start .wrapper .options-footer-elements .options"
+	".start .wrapper .options-footer-elements .options"
 );
 
 let checkAllGithub = document.getElementById("option1"); //Родительский чекбокс гитхаба
@@ -38,28 +38,29 @@ let checkAllJenkins = document.getElementById("option3"); //Родительск
 let checkBoxesJenkins = document.querySelectorAll("input.jenkins"); //Дочерний чекбокс
 
 function selectingAllNestedCheckboxes(parents, childs, className) {
-  for (let i = 0; i < childs.length; i++) {
-    childs[i].onclick = function () {
-      let checkedCount = document.querySelectorAll(
-        `input.${className}:checked`
-      ).length;
+	for (let i = 0; i < childs.length; i++) {
+		childs[i].onclick = function () {
+			let checkedCount = document.querySelectorAll(
+				`input.${className}:checked`
+			).length;
 
-      parents.checked = checkedCount > 0;
-      parents.indeterminate = checkedCount > 0 && checkedCount < childs.length;
-    };
-  }
+			parents.checked = checkedCount > 0;
+			parents.indeterminate =
+				checkedCount > 0 && checkedCount < childs.length;
+		};
+	}
 
-  parents.onclick = function () {
-    for (let i = 0; i < childs.length; i++) {
-      childs[i].checked = this.checked;
-    }
-  };
+	parents.onclick = function () {
+		for (let i = 0; i < childs.length; i++) {
+			childs[i].checked = this.checked;
+		}
+	};
 }
 
 // Скрытие-показ инфы
 informationTrigger.addEventListener("click", informationToggle);
 function informationToggle() {
-  informationSection.classList.toggle("active");
+	informationSection.classList.toggle("active");
 }
 
 // Скрытие-показ опций
@@ -68,47 +69,48 @@ mainFormSectionTrigger.addEventListener("click", calcContentHeight);
 mainFormSectionTrigger2.addEventListener("click", mainFormSectionToggle);
 mainFormSectionTrigger2.addEventListener("click", calcContentHeight);
 function mainFormSectionToggle() {
-  mainFormSection.classList.toggle("cards-active");
+	mainFormSection.classList.toggle("cards-active");
 }
 
 function calcContentHeight() {
-  let firstBlockSize = document.querySelector(".wrapper.block-first");
-  let secondBlockSize = document.querySelector(".wrapper.block-second");
-  let cardsActiveCheck = document.querySelector(
-    ".main-block__page.cards-active"
-  );
-  let secondStepCheck = document.querySelector(".start.hidden");
-  if (secondStepCheck !== null) {
-    appBlock.style.height =
-      "calc(" + secondBlockSize.offsetHeight + "px + 2em)";
-  } else if (cardsActiveCheck !== null) {
-    appBlock.style.height = "calc(" + firstBlockSize.offsetHeight + "px + 3em)";
-  } else {
-    appBlock.style.height =
-      "calc(" + mainFormSize.offsetHeight + "px + 1.5rem)";
-  }
+	let firstBlockSize = document.querySelector(".wrapper.block-first");
+	let secondBlockSize = document.querySelector(".wrapper.block-second");
+	let cardsActiveCheck = document.querySelector(
+		".main-block__page.cards-active"
+	);
+	let secondStepCheck = document.querySelector(".start.hidden");
+	if (secondStepCheck !== null) {
+		appBlock.style.height =
+			"calc(" + secondBlockSize.offsetHeight + "px + 2em)";
+	} else if (cardsActiveCheck !== null) {
+		appBlock.style.height =
+			"calc(" + firstBlockSize.offsetHeight + "px + 3em)";
+	} else {
+		appBlock.style.height =
+			"calc(" + mainFormSize.offsetHeight + "px + 1.5rem)";
+	}
 }
 
 export {
-  informationToggle,
-  mainFormSectionToggle,
-  calcContentHeight,
-  mainBtn,
-  mainBtn2,
-  appBlock,
-  mainFormSize,
-  body,
-  mainForm,
-  modalBtn,
-  modalBtnClose,
-  optionsBtn,
-  informationSection,
-  titleTextarea,
-  consoleContainer,
-  consoleContainerContent,
-  selectingAllNestedCheckboxes,
-  checkAllGithub,
-  checkBoxesGithub,
-  checkAllJenkins,
-  checkBoxesJenkins,
+	informationToggle,
+	mainFormSectionToggle,
+	calcContentHeight,
+	mainBtn,
+	mainBtn2,
+	appBlock,
+	mainFormSize,
+	body,
+	mainForm,
+	modalBtn,
+	modalBtnClose,
+	optionsBtn,
+	informationSection,
+	titleTextarea,
+	consoleContainer,
+	consoleContainerContent,
+	selectingAllNestedCheckboxes,
+	checkAllGithub,
+	checkBoxesGithub,
+	checkAllJenkins,
+	checkBoxesJenkins,
 };
--- src/js/onCreateTestCaseStorage.js
+++ src/js/onCreateTestCaseStorage.js
@@ -12,68 +12,82 @@ const createTestCaseField = document.querySelector("#tc_title");
 const createStepField = document.querySelector("#text_case");
 const createTestCaseButton = document.querySelector("#add_test_case__submit");
 
-
 tasks.forEach((data) => {
-  onCreateTestCase({ data });
+	onCreateTestCase({ data });
 });
 
 createTestCaseField.addEventListener("input", () => {
-  createTestCaseButton.disabled = !createTestCaseField.value;
+	createTestCaseButton.disabled = !createTestCaseField.value;
 });
 
 createStepField.addEventListener("input", () => {
-  createTestCaseButton.disabled = !createStepField.value;
+	createTestCaseButton.disabled = !createStepField.value;
 });
 
 createTestCaseForm.addEventListener("submit", (event) => {
-  event.preventDefault();
+	event.preventDefault();
 
-  const title = createTestCaseField.value;
-  const steps = createStepField.value;
+	const title = createTestCaseField.value;
+	const steps = createStepField.value;
 
-  if (title) {
-    const data = {
-      title,
-      steps,
-      timestamp: new Date().getTime(),
-    };
+	if (title) {
+		const data = {
+			title,
+			steps,
+			timestamp: new Date().getTime(),
+		};
 
-    storage.create(data);
+		storage.create(data);
 
-    onCreateTestCase({ data });
+		onCreateTestCase({ data });
 
-    createTestCaseForm.reset();
-  }
+		createTestCaseForm.reset();
+	}
 });
 
-function onCreateTestCase({ data }) {
-  const clone = template.content.cloneNode(true);
-  const testCase = clone.querySelector(".task");
-  const title = clone.querySelector(".task__text");
-  const step = clone.querySelector(".step__text");
-  const remove = clone.querySelector(".task__delete");
-
-  title.innerHTML = data.title;
-  step.innerHTML = data.steps;
-
-  title.addEventListener("input", () => {
-    data.title = title.innerHTML;
-
-    storage.update(data);
-  });
-  step.addEventListener("input", () => {
-    data.steps = step.innerHTML;
-
-    storage.update(data);
-  });
-
-  remove.addEventListener("click", () => {
-    storage.delete(data);
-
-    testCase.remove();
-  });
-
-  container.appendChild(clone);
+// Кнопки добавленных тест-кейсов
+function tcValueEdit() {
+	let tcEditBtn = document.querySelector(".btn-edit");
+	let tcSaveBtn = document.querySelector(".btn-save");
+	let taskEditing = document.querySelector(".tasks__task");
+	tcEditBtn.addEventListener("click", taskActive);
+	tcSaveBtn.addEventListener("click", taskSave);
+	function taskActive() {
+		taskEditing.classList.add("active");
+	}
+	function taskSave() {
+		taskEditing.classList.remove("active");
+	}
 }
 
- 
+function onCreateTestCase({ data }) {
+	const clone = template.content.cloneNode(true);
+	const testCase = clone.querySelector(".task");
+	const title = clone.querySelector(".task__text");
+	const step = clone.querySelector(".step__text");
+	const remove = clone.querySelector(".btn-delete");
+
+	title.innerHTML = data.title;
+	step.innerHTML = data.steps;
+
+	title.addEventListener("input", () => {
+		data.title = title.innerHTML;
+
+		storage.update(data);
+	});
+	step.addEventListener("input", () => {
+		data.steps = step.innerHTML;
+
+		storage.update(data);
+	});
+
+	remove.addEventListener("click", () => {
+		storage.delete(data);
+
+		testCase.remove();
+	});
+
+	container.appendChild(clone);
+}
+
+tcValueEdit();
