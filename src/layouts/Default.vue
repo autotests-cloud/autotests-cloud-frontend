ed6e80d7d877dd3870afd04cc6d3897e838c6e20
Robert Isaev <robert.isaev@gmail.com>
2021-10-06 16:47:46 +0200

    gridsome starter with tailwind

---
+++ src/layouts/Default.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="layout container">
+    <header class="relative py-10 header">
+      <div class="flex items-center justify-between">
+        <g-link
+          to="/"
+          class="flex-shrink-0"
+        >
+          <h1 class="text-xl font-bold">Autotests.cloud</h1>
+        </g-link>
+
+        <nav class="font-bold font-lg">
+          <g-link
+            to="/"
+            active-class="is-active"
+            class="mr-3"
+          >
+            Home page
+          </g-link>
+
+          <g-link
+            to="/"
+            active-class="is-active"
+            class="mr-3"
+          >
+          Telegram
+          </g-link>
+
+          <g-link
+            to="/"
+            active-class="is-active"
+          >
+            GitHub
+          </g-link>
+        </nav>
+      </div>
+    </header>
+
+    <slot />
+
+    <footer class="p-10 mt-10 md:mt-28 md:p">
+      <p class="text-center">
+        © 2015-{{ currentYear }} Autotests Cloud.
+      </p>
+    </footer>
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName,
+    siteDescription,
+  }
+}
+</static-query>
+
+<script>
+
+export default {
+  metaInfo() {
+    return this.$seo({
+      description: this.$static.metadata.siteDescription,
+      openGraph: {
+        description: this.$static.metadata.siteDescription,
+        image: {
+          url: '/', // @todo add og image
+        },
+      },
+    });
+  },
+  data() {
+    return {
+      currentYear: new Date().getFullYear(),
+    };
+  },
+};
+</script>
+
+<style>
+body {
+  padding: 0;
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,26 @@
+<template>
+  <layout clas="page-home">
+    <p class="text-xl font-bold">Стек</p>
+    <p class="text-xl font-bold">Конфигурация</p>
+    <p class="text-xl font-bold">Форма</p>
+    <p class="text-xl font-bold">Поддержка</p>
+  </layout>
+</template>
+
+<page-query>
+query {
+  metadata {
+    siteName,
+  }
+}
+</page-query>
+
+<script>
+
+export default {
+  components: { }
+};
+</script>
+
+<style>
+</style>
